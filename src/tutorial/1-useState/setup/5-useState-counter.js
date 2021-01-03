dc82d6ea517d4398751b5f2eb4dc9c67b4c8363d
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-03 16:08:13 +0100

    useState Complex Counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,6 +6,16 @@ const UseStateCounter = () => {
     const decreaseHandler = () => {
         setValue(value - 1);
     }
+
+    const complexIncrease = () => {
+        setTimeout(() => {
+            //setValue(value + 1)
+            setValue((prevState) => {
+                return prevState + 1;
+            })
+        }, 2000)
+    }
+
   return <>
     <section style={{margin: '4rem 0'}}>
         <h2>regular counter</h2>
@@ -13,6 +23,13 @@ const UseStateCounter = () => {
         <button className="btn" onClick={decreaseHandler}>decrease</button>
         <button className="btn" onClick={() => setValue(0)}>reset</button>
         <button className="btn" onClick={() => setValue(value + 1)}>increase</button>
+    </section>
+
+    <section style={{margin: '4rem 0'}}>
+        <h2>More complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>increase later</button>
+        
     </section>
   </>;
 };
