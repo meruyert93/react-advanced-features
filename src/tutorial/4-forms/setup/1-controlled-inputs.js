a0dff782a6198c7c09da64a735a50c34a0bb319e
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-06 21:43:40 +0100

    Controlled inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,28 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('hello')
+    }
+  return (
+    <>
+        <article>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="firstName">Name: </label>
+                    <input type="text" id="firstName" name="firstName"></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">Email: </label>
+                    <input type="text" id="email" name="email"></input>
+                </div>
+                <button type="submit">add person</button>
+            </form>
+        </article>
+    </>
+);
 };
 
 export default ControlledInputs;
