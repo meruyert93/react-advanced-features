22ccec8cde1324bd1855c09d09b871f0eb230204
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-10 18:51:27 +0100

    React Router DOM

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -2,9 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
 const Person = () => {
+    const {id} = useParams();
+    const[name, setName] = useState('default name')
+
+    useEffect(() => {
+        const newPerson = data.find((person) => {
+            return person.id === parseInt(id) ;
+        });
+        setName(newPerson.name)
+    }, [])
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
+      <Link to="/people" className="btn">
+          Back to People
+      </Link>
     </div>
   );
 };
